d74bdd0072a90966c3857786f8d147a071834cf6
Dimitar Manastirsky <dmanastirski@gmail.com>
2024-10-17 23:37:36 +0200

    changed/added file and folder architecture|main page responsiveness

---
+++ pages/main/scripts/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afe43c93460c626a492dca3c245e5d8ea3c4c0f4c18b5e1b9ef9b77c6e7f7cad
+size 236
--- script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.querySelector('.toggle')
-        const showcase = document.querySelector('.showcase')
-        const text = document.querySelector('.text')
-
-        menuToggle.addEventListener('click', () => {
-            menuToggle.classList.toggle('active')
-            showcase.classList.toggle('active')
-            text.classList.toggle('active')
-        })
